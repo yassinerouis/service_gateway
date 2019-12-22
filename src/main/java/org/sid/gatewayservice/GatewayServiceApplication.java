461b4173382cd3032b5c872e64064dab038a4ccc
yassinerouis <56519992+yassinerouis@users.noreply.github.com>
2019-12-22 12:49:17 +0100

    first commit

---
+++ src/main/java/org/sid/gatewayservice/GatewayServiceApplication.java
@@ -0,0 +1,15 @@
+package org.sid.gatewayservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class GatewayServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GatewayServiceApplication.class, args);
+	}
+
+}
